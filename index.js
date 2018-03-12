77e2f7c690f506a6468bd7fe9c6f5dcb75675258
Jordan Merrill <amanithought@gmail.com>
2018-03-12 19:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string){
     return "I can't hear you"
   }
+  if (string.toUpperCase() === string){
+    return "I can't hear you"
+  }
   
  
   
